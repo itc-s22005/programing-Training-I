0d78716d903d5a87527fda336e0d90bac774d5cd
itc-s22005 <104543474+itc-s22005@users.noreply.github.com>
2022-09-29 01:36:48 +0900

    Update block.py

--- block.py
+++ block.py
@@ -1,1 +1,16 @@
+class Block:
+
+    def __init__(self, x, y, width, height):
+        '''ブロック作成'''
+
+        # 中心座標とサイズを設定
+        self.x = x
+        self.y = y
+        self.w = width
+        self.h = height
+
+    def getCoords(self):
+        '''左上の座標と右下の座標の取得'''
+
+        return (self.x - self.w / 2, self.y - self.h / 2, self.x + self.w / 2, self.y + self.h / 2)
 
